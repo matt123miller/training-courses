33f204cc0572de087a279381becec609ad0e4ca1
Matt Miller <matt.miller@simpleclick.co.uk>
2019-08-01 09:07:03 +0100

    nodejs unit testing

---
+++ nodejs-unit-testing/test/test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+describe('file to test', (params) => {
+    context('function to test',(params) => {
+        it('should do this specific thing', (params) => {
+            assert.equal(1,1);
+        });
+
+        it('will be written later');
+    });
+});
