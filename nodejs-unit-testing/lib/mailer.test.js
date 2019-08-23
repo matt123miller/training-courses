d28f89912ebf4d69328e172479cddc3490f24130
Matt Miller <matt123miller@hotmail.co.uk>
2019-08-23 11:14:13 +0100

    Wrote the last mailer tests.

--- nodejs-unit-testing/lib/mailer.test.js
+++ nodejs-unit-testing/lib/mailer.test.js
@@ -51,8 +51,28 @@ describe('mailer', () => {
     context('sendPasswordResetEmail', () => {
 
         // Basically the same as sendWelcome email
-        it('', () => {
+        it('should throw error with invalid args', async () => {
 
+            expect(mailer.sendPasswordResetEmail()).to.eventually.be.rejectedWith('Invalid input');
+        });
+
+        it('should return a promise, whether it fails or not.', () => {
+
+            expect(mailer.sendPasswordResetEmail()).is.instanceOf(Promise);
+            expect(mailer.sendPasswordResetEmail('a@b.com')).is.instanceOf(Promise);
+        });
+
+        it('Should send an email', async () => {
+
+            let sendEmailStub = sandbox.stub().resolves('fake email sent');
+            // Replace the private sendEmail function with our stub
+            mailer.__set__('sendEmail', sendEmailStub);
+
+            await mailer.sendPasswordResetEmail('a@b.com');
+            expect(sendEmailStub).to.have.been.calledOnce;
+            expect(sendEmailStub).to.have.been.calledWith('a@b.com', 'Please click http://some_link to reset your password.');
+
+            await expect(mailer.sendPasswordResetEmail('a@b.com')).to.eventually.equal('fake email sent')
         });
     });
 
